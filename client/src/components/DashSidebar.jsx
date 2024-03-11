95e7872c82f243bbff280f5a7051c1c1e4637d4b
UmeshSandeepa <umeshsandeepa373@gmail.com>
2024-03-11 22:51:58 +0530

    Add signout functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,10 +1,15 @@
 import { Sidebar, SidebarItem, SidebarItemGroup } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { HiArrowSmRight, HiUser } from "react-icons/hi";
+import { useDispatch } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
+import { signoutSuccess } from "../redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 export default function DashSidebar() {
   const location = useLocation();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [tab, setTab] = useState("");
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -13,6 +18,24 @@ export default function DashSidebar() {
       setTab(tabFromUrl);
     }
   }, [location.search]);
+
+  const handleSignOut = async () => {
+    try {
+      const res = await fetch("api/user/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+        navigate("/SignIn");
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <Sidebar className="w-full md:w-56">
       <SidebarItemGroup>
@@ -22,12 +45,17 @@ export default function DashSidebar() {
             icon={HiUser}
             label={"User"}
             labelColor="dark"
-            as = 'div'
+            as="div"
           >
             Profile
           </SidebarItem>
         </Link>
-        <SidebarItem active icon={HiArrowSmRight} className="cursor-pointer">
+        <SidebarItem
+          active
+          icon={HiArrowSmRight}
+          className="cursor-pointer"
+          onClick={handleSignOut}
+        >
           Sign Out
         </SidebarItem>
       </SidebarItemGroup>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,46 @@
-import { Avatar, Button, Dropdown, DropdownDivider, DropdownItem, Navbar, NavbarToggle, TextInput } from "flowbite-react";
+import {
+  Avatar,
+  Button,
+  Dropdown,
+  DropdownDivider,
+  DropdownItem,
+  Navbar,
+  NavbarToggle,
+  TextInput,
+} from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSun } from "react-icons/fa";
-import { useSelector, useDispatch} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { toggleTheme } from "../redux/theme/themeSlice";
+import { signoutSuccess } from "../redux/user/userSlice";
+import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
-    const path = useLocation().pathname
-    const dispatch = useDispatch()
-    const {currentUser} = useSelector(state => state.user)
-    const {theme} = useSelector((state) => state.theme)
+  const path = useLocation().pathname;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { currentUser } = useSelector((state) => state.user);
+  const { theme } = useSelector((state) => state.theme);
+
+  const handleSignOut = async () => {
+    try {
+      const res = await fetch('api/user/signout', {
+        method: 'POST' ,
+      })
+      const data = await res.json()
+      if(!res.ok){
+        console.log(data.message);
+      }else{
+        dispatch(signoutSuccess())
+        navigate('/SignIn');
+      }      
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
   return (
     <Navbar className="border-b-2">
       <Link
@@ -33,40 +64,53 @@ export default function Header() {
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill onClick={()=>dispatch(toggleTheme())}>
-          {theme === 'light' ? <FaSun/> : <FaMoon/>}
+        <Button
+          className="w-12 h-10 hidden sm:inline"
+          color="gray"
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+          {theme === "light" ? <FaSun /> : <FaMoon />}
         </Button>
-        
-          {currentUser ? (
-            <Dropdown arrowIcon={false} inline label={
-              <Avatar alt='user' img={currentUser.profilePicture} rounded/>
-            }>
-              <Dropdown.Header>
-                <span className="block text-sm">@{currentUser.username}</span>
-                <span className="block text-sm font-medium truncate">{currentUser.email}</span>
-              </Dropdown.Header>
-              <Link to = {'/Dashboard?tab=profile'}>
-                <DropdownItem>Profile</DropdownItem>
-              </Link>
-              <DropdownDivider/>
-              <DropdownItem>Sign Out</DropdownItem>
-            </Dropdown>
-          ) : (
-            <Link to="/SignIn">
-            <Button gradientDuoTone="purpleToBlue" outline>Sign In</Button>
-        </Link>
-          ) }
-          
-        <NavbarToggle/>
+
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt="user" img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-sm font-medium truncate">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={"/Dashboard?tab=profile"}>
+              <DropdownItem>Profile</DropdownItem>
+            </Link>
+            <DropdownDivider />
+            <DropdownItem onClick={handleSignOut}>Sign Out</DropdownItem>
+          </Dropdown>
+        ) : (
+          <Link to="/SignIn">
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
+
+        <NavbarToggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link active={path==="/"} as={'div'}>
+        <Navbar.Link active={path === "/"} as={"div"}>
           <Link to="/">Home</Link>
         </Navbar.Link>
-        <Navbar.Link active={path==="/About"} as={'div'}>
+        <Navbar.Link active={path === "/About"} as={"div"}>
           <Link to="/About">About</Link>
         </Navbar.Link>
-        <Navbar.Link active={path==="/Projects"} as={'div'}>
+        <Navbar.Link active={path === "/Projects"} as={"div"}>
           <Link to="/Projects">Projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
